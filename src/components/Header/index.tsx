3e64b3b5c525bb9b69eb4b5ab020027283ead70e
yigit <mehmetyigityalim@gmail.com>
2023-09-23 14:19:20 +0300

    debugging

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -125,7 +125,7 @@ function DesktopMenu(): React.JSX.Element {
 
     return (
         <header className='flex items-center justify-between p-6'>
-            <h1 className='text-4xl font-black text-start italic'>LO</h1>
+            <h1 className='text-4xl font-black text-start italic'>Mesut.</h1>
             <div
                 className={cn(
                     'flex flex-row gap-x-2 items-center justify-center',
